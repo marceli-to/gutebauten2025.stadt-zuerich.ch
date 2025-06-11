107166358eb37120967c37e4fac3423e738126b3
Marcel Stadelmann <m@marceli.to>
2025-06-11 17:41:46 +0200

    wip

---
+++ resources/views/components/animation.blade.php
@@ -0,0 +1,18 @@
+<div class="">
+  <lottie-player
+    id="lottiePlayer"
+    class="w-full hidden md:block"
+    autoplay
+    loop
+    src="{{ asset('media/animation-lg.json') }}">
+  </lottie-player>
+  <lottie-player
+    id="lottiePlayer"
+    class="w-full md:hidden"
+    autoplay
+    loop
+    src="{{ asset('media/animation.json') }}">
+  </lottie-player>
+</div>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+  
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.guest')
 @section('seo_description', 'Auszeichnung für gute Bauten der Stadt Zürich 2021–2024')
 @section('content')
-<div class="w-full min-h-dvh md:min-h-screen pt-30 md:pt-20 lg:pt-25 xl:pt-30">
+<div class="w-full pt-30 md:pt-20 lg:pt-25 xl:pt-30">
   
   <x-layout.container class="relative">
     <div class="md:absolute md:top-10 md:right-20 mb-30 md:mb-0 z-10">
@@ -11,8 +11,7 @@
     </div>
   </x-layout.container>
   
-  <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.3.0/dist/dotlottie-wc.js" type="module"></script>
-  <dotlottie-wc src="https://lottie.host/7bd63275-7645-428a-9307-504506df319e/YV2l85vwYp.lottie" autoplay loop></dotlottie-wc>
+  <x-animation />
 
   <div class="bg-white h-60 lg:h-100 max-w-[90%] md:max-w-[340px] lg:max-w-[520px] xl:max-w-[695px] flex items-start justify-center pt-10 lg:pt-20">
     <x-icons.logo class="w-120 lg:w-[228px] xl:w-[245px]" />
