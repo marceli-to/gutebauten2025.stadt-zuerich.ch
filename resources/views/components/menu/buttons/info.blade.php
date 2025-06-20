affa8ad45ba8df07ee1a2d11c82d92172524601d
Marcel Stadelmann <m@marceli.to>
2025-06-20 17:56:28 +0200

    wip

--- resources/views/components/menu/buttons/info.blade.php
+++ resources/views/components/menu/buttons/info.blade.php
@@ -1,14 +1,14 @@
 <a 
   href="{{ route('page.info') }}"  
   title="Info zum Voting" 
-  class="relative shrink-0 flex justify-center items-center aspect-[353/350] md:aspect-square max-w-[280px] xs:max-w-[330px] md:max-w-[400px] xl:max-w-[500px] w-full text-white hover:text-lumora transition-color duration-300">
+  class="relative shrink-0 flex justify-center items-center aspect-[353/350] lg:aspect-square max-w-[280px] xs:max-w-[330px] lg:max-w-[45%] w-full text-white hover:text-lumora transition-color duration-300">
   
-  <svg width="353" height="350" viewBox="0 0 353 350" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute block w-full h-full z-10 md:hidden">
+  <svg width="353" height="350" viewBox="0 0 353 350" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute block w-full h-full z-10 lg:hidden">
     <path d="M207.164 86.2969L208.219 87.0547L209.341 86.3984L351 3.4873V347.5H2V85.5381L90.5498 2.58301L207.164 86.2969Z" fill="currentColor" stroke="black" stroke-width="4"/>
   </svg>
 
-  <svg width="716" height="716" viewBox="0 0 716 716" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute hidden md:block w-full h-full z-10">
-    <circle cx="357.591" cy="357.591" r="355.591" fill="currentColor" stroke="black" stroke-width="4"/>
+  <svg width="716" height="716" viewBox="0 0 716 716" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute hidden lg:block w-full h-full z-10">
+    <circle cx="357.591" cy="357.591" r="355.591" fill="currentColor" stroke="black" stroke-width="5"/>
   </svg>
 
   <span class="block text-black text-2xl relative z-20">
