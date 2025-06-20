527063eace96ad34014649ece107a95c754420df
Marcel Stadelmann <m@marceli.to>
2025-06-20 14:16:51 +0200

    wip

--- resources/js/modules/ImageSlider.js
+++ resources/js/modules/ImageSlider.js
@@ -57,7 +57,7 @@ export default class ImageSlider {
       this.isPaused = true;
       
       // Hide container during resize
-      gsap.to(this.container, { opacity: 0, duration: 0.2 });
+      gsap.to(this.container, { opacity: 0, duration: 0.1 });
       
       resizeTimeout = setTimeout(() => {
         this.rebuildSlider();
@@ -104,7 +104,7 @@ export default class ImageSlider {
     this.isPaused = false;
     
     // Show container again after rebuild is complete
-    gsap.to(this.container, { opacity: 1, duration: 0.3 });
+    gsap.to(this.container, { opacity: 1, duration: 0.2 });
     
     // Debug output
     this.debugResize();
