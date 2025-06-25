6db02a2d9bf6dcf8eba5d59ba8d49e5127dc2fd7
Marcel Stadelmann <m@marceli.to>
2025-06-25 14:06:40 +0200

    wip

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,4 +1,4 @@
-<header class="sticky top-0 z-[2000] bg-lumora xl:bg-red-200 2xl:bg-blue-200 min-h-52 pb-5 pt-2 md:py-0 md:min-h-60 xl:min-h-72 flex items-center border-b-3 xl:border-b-4 border-black">
+<header class="sticky top-0 z-[2000] bg-lumora lg:bg-green-200 xl:bg-red-200 2xl:bg-blue-200 min-h-52 pb-5 pt-2 md:py-0 md:min-h-60 xl:min-h-72 flex items-center border-b-3 xl:border-b-4 border-black">
   <x-layout.container class="flex justify-between items-start md:items-center">
     {{ $slot }}
     @if (!request()->routeIs('page.building'))
