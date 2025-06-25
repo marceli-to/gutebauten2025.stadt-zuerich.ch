62023d64c976e052fe2dba321a7d64470b4cb46c
Marcel Stadelmann <m@marceli.to>
2025-06-25 14:38:49 +0200

    wip

--- resources/js/modules/ImageSlider.js
+++ resources/js/modules/ImageSlider.js
@@ -62,20 +62,34 @@ export default class ImageSlider {
 
   setupResizeObserver() {
     let resizeTimeout;
-
-    const resizeObserver = new ResizeObserver(() => {
+    let lastWidth = 0;
+  
+    const resizeObserver = new ResizeObserver((entries) => {
       if (!this.hasInitialized) return;
-
+  
+      const entry = entries[0];
+      const currentWidth = entry.contentRect.width;
+      
+      // Only respond to width changes, completely ignore height
+      if (lastWidth === 0) {
+        lastWidth = currentWidth;
+        return;
+      }
+      
+      if (Math.abs(currentWidth - lastWidth) < 1) return;
+      
+      lastWidth = currentWidth;
+  
       clearTimeout(resizeTimeout);
       this.isPaused = true;
-
+  
       gsap.to(this.container, { opacity: 0, duration: 0.2 });
-
+  
       resizeTimeout = setTimeout(() => {
         this.rebuildSlider();
-      }, 500);
+      }, 150);
     });
-
+  
     resizeObserver.observe(this.container);
   }
 
