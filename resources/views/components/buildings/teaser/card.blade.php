59c44d49be90e00666a178e6d55d3b5f050740c9
Marcel Stadelmann <m@marceli.to>
2025-06-26 23:21:19 +0200

    wip

--- resources/views/components/buildings/teaser/card.blade.php
+++ resources/views/components/buildings/teaser/card.blade.php
@@ -1,6 +1,5 @@
 <a 
   href="{{ route('page.building', $building->slug) }}"
-  title="{{ $building->title }}"
   aria-label="{{ $building->title }}"
   class="relative block group">
   <figure>
@@ -14,7 +13,10 @@
         loading="lazy" 
         {{ $attributes->merge(['class' => 'w-full h-auto']) }}>
     </picture>
-    <figcaption class="text-lumora lg:p-20 hyphens-auto text-xl md:text-2xl text-center absolute inset-0 flex items-center justify-center text-balance md:opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+    @if ($building->slug == 'guggach-siedlung-hofwiesenstrasse')
+      @include('components.buildings.teaser.shapes.' . $building->slug, ['class' => 'hidden md:block absolute inset-0 w-full h-full z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200'])
+    @endif
+    <figcaption class="text-lumora lg:p-20 hyphens-auto text-xl md:text-2xl md:text-black text-center absolute inset-0 z-20 flex items-center justify-center text-balance md:opacity-0 group-hover:opacity-100 transition-opacity duration-200">
       {{ $building->title }}
     </figcaption>
   </figure>
---
+++ resources/views/components/buildings/teaser/shapes/guggach-siedlung-hofwiesenstrasse.blade.php
@@ -0,0 +1,3 @@
+<svg width="360" height="480" viewBox="0 0 360 480" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ $class }}">
+<path d="M266.542 5.5L354.5 127.504V474.5H5.5V5.5H266.542Z" fill="white" stroke="black" stroke-width="3"/>
+</svg>
